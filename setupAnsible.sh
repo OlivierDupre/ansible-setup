5c427ed377f89df91f1d619374951e1aa1a3f564
Olivier Dupré <dupreolivier@gmail.com>
2019-01-30 18:17:51 +0100

    Added basic ansible setup

---
+++ setupAnsible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# INSTALL https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-ubuntu
+sudo apt install -y software-properties-common openssh-server
+sudo apt-add-repository --yes  ppa:ansible/ansible
+sudo apt update -y && sudo apt install -y ansible
+
+# SETUP https://docs.ansible.com/ansible/latest/user_guide/intro_getting_started.html
+echo "127.0.0.1" | sudo tee /etc/ansible/hosts
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -f ~/.ssh/id_rsa -q -N ''
+  ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@127.0.0.1
+fi
+
+ssh-agent bash
+ssh-add ~/.ssh/id_rsa
+
+ansible all -m ping
