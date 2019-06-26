f71989f4a9068b663fd9d376a6f4d1d988950273
Olivier Dupré <dupreolivier@gmail.com>
2019-06-26 13:37:34 +0200

    Added function to connect to remote GCP VM and start it if necessary

--- gcp-instance-setup.sh
+++ gcp-instance-setup.sh
@@ -3,15 +3,20 @@
 gcloud kms keyrings create perso  --location global
 gcloud kms keys create perso --location global --keyring perso  --purpose encryption
 
+gcloud compute disks create disk1 --image-project ubuntu-os-cloud --image-family ubuntu-1904 --zone ${GCP_ZONE} #--size 20
 
-gcloud compute disks create disk1 --image-project ubuntu-os-cloud 	 --image-family ubuntu-1904 --zone europe-west1-b #--size 20
 gcloud compute images create nested-ubuntu-image \
-  --source-disk disk1 --source-disk-zone europe-west1-b \
+  --source-disk disk1 --source-disk-zone ${GCP_ZONE} \
   --licenses "https://www.googleapis.com/compute/v1/projects/vm-options/global/licenses/enable-vmx"
-gcloud compute instances create dev-env --zone europe-west1-b \
+
+gcloud compute instances create ${INSTANCE_NAME}
+              --zone ${GCP_ZONE} \
               --min-cpu-platform "Intel Skylake" \
               --image nested-ubuntu-image \
               --description "Personal development environment" \
               --boot-disk-size 20GB \
               --boot-disk-type pd-ssd \
+              --metadata enable-oslogin=FALSE
               --machine-type n1-standard-4  # 4 CPU + 16 Gb RAM
+
+gcloud compute disks snapshot ${INSTANCE_NAME} --project=${PROJECT_NAME} --description=Kata-container,\ Docker\ \&\ gVisor\ installed.$'\n'microk8s,\ gcloud,\ aws-cli,\ az-cli\ installed. --labels=project=perso,env=dev --snapshot-names=initial-snapshot --zone=${GCP_ZONE} --storage-location=europe-west1
