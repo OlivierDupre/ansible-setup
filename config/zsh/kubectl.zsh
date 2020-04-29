0f2c30060be5c12e86a0273a5653437797cda8bc
Olivier Dupré <dupreolivier@gmail.com>
2020-04-30 00:43:55 +0200

    Renamed

--- config/zsh/kubectl.zsh
+++ config/zsh/kubectl.zsh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
+alias docker='docker.exe'
+alias docker-compose='docker-compose.exe'
+alias kubectl='kubectl.exe'
 alias k='kubectl'
 alias ki='kubectl --insecure-skip-tls-verify=true'
 alias kir='kubectl --insecure-skip-tls-verify=true --field-selector "status.phase==Running"'
+alias kctx='kubectx'
+alias kns='kubens'
 
 if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi
 
