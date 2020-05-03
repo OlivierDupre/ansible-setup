2c2fe9367b3bf073d4f8e0f66e6ed7f871031c65
Olivier Dupré <dupreolivier@gmail.com>
2020-05-03 23:51:31 +0200

    Improved script, made them resilient. Share status properly

--- config/aws.zsh
+++ config/aws.zsh
@@ -27,9 +27,9 @@ function awx() {
 
 alias awsctx=awx
 
-function statusEc3(){
-    echoStatus=${1:-false}
-
+function statusEc2(){
+    echoStatus=${1:-true}
+  
     [ -z ${AWS_REGION} ] && echo "Context unset. First run awsctx" && return 1
 
     ec2_instance=`aws ec2 describe-instances --region ${AWS_REGION}  --filters ${EC2_FILTERS} --query "Reservations[0].Instances[0].InstanceId" | jq -r '.'`
@@ -43,11 +43,13 @@ function statusEc3(){
         fi
     fi
 
-    [ $echoStatus ] && echo "Instance ${ec2_instance}: $ec2_state"
+    if $echoStatus; then echo "Instance ${ec2_instance}: $ec2_state"; fi
+
+    return 0
 }
 
-function startEc3(){
-    statusEc3 true
+function startEc2(){
+    statusEc2 false
     result=$?
     [ ! $result -eq 0 ] && return $result
     echo "Ec2 ${ec2_instance} status: $ec2_state"
@@ -57,87 +59,61 @@ function startEc3(){
         echo "Starting Ec2 ${ec2_instance}"
         awsCall=$(aws ec2 start-instances  --instance-ids ${ec2_instance} --region ${AWS_REGION})
     else
-        echo "Ec2 ${ec2_instance} not in a startable state"
-    fi
-}
-
-function statusEc2(){
-    [ -z ${AWS_REGION} ] && echo "Context unset. First run awsctx" && return 1
-
-    export EC2_INSTANCE=`aws ec2 describe-instances --region ${AWS_REGION}  --filters ${EC2_FILTERS} --query "Reservations[0].Instances[0].InstanceId" | jq -r '.'`
-    EC2_STATE=`aws ec2 describe-instances --region ${AWS_REGION}  --filters ${EC2_FILTERS} --query "Reservations[0].Instances[0].State.Name" | jq -r '.'`
-    
-    if [ $EC2_STATE = "running" ]; then 
-        EC2_STATUS=`aws ec2 describe-instance-status  --instance-ids ${EC2_INSTANCE} --region ${AWS_REGION} --query "InstanceStatuses[0].InstanceStatus.Status" | jq -r '.'`;
-        if [ $EC2_STATUS != "ok" ]; then
-            EC2_STATE=$EC2_STATUS
-        fi
+        [ "x$ec2_state" != "xrunning" ] && echo "Ec2 ${ec2_instance} not in a startable state"
     fi
 
-    echo $EC2_STATE
-}
-
-function startEc2(){
-    STATUS=$(statusEc2)
-    RESULT=$?
-    [ ! $RESULT -eq 0 ] && echo $STATUS && return $RESULT
-    echo "Status: $STATUS"
-
-    if [ "x$STATUS" != "xrunning" ]
-    then
-        echo "Starting Ec2 ${EC2_INSTANCE}"
-        aws ec2 start-instances  --instance-ids ${EC2_INSTANCE} --region ${AWS_REGION}
-    else
-        echo "Ec2 ${EC2_INSTANCE} already running"
-    fi
+    return 0
 }
 
 function stopEc2(){
-    STATUS=$(statusEc2)
-    RESULT=$?
-    [ ! $RESULT -eq 0 ] && echo $STATUS && return $RESULT
-    echo "Status: $STATUS"
+    statusEc2 false
+    result=$?
+    [ ! $result -eq 0 ] && return $result
+    echo "Ec2 ${ec2_instance} status: $ec2_state"
 
-    if [ "x$STATUS" = "xrunning" ]
+    if [ "x$ec2_state" = "xrunning" ]
     then
-        echo "Stopping Ec2 ${EC2_INSTANCE}"
-        aws ec2 stop-instances  --instance-ids ${EC2_INSTANCE} --region ${AWS_REGION}
+        echo "Stopping Ec2 ${ec2_instance}"
+        awsCall=$(aws ec2 stop-instances  --instance-ids ${ec2_instance} --region ${AWS_REGION})
     else
-        echo "Ec2 ${EC2_INSTANCE} not running"
+        [ "x$ec2_state" != "xstopped" ] && echo "Ec2 ${ec2_instance} not in a stoppable state"
     fi
+
+    return 0
 }
 
 function sshEc2() {
     # export AWS_KEY=~/.ssh/mykey.pem
     # export EC2_FILTERS=file:///my/absolute/path/to/filters.json   # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
 
-    STATUS=$(statusEc2)
-    RESULT=$?
-    [ ! $RESULT -eq 0 ] && echo $STATUS && return $RESULT
-    echo "Status: $STATUS"
+    statusEc2 false
+    result=$?
+    [ ! $result -eq 0 ] && return $result
 
-    if [ "x$STATUS" = "xstopping" ]
+    if [ "x$ec2_state" = "xstopping" ]
     then
-        echo "Cannot ssh to a stopping instance"
+        echo "Cannot ssh to Ec2 ${ec2_instance} status: $ec2_state"
         return 50
     fi
 
-    if [ "x$STATUS" = "xstopped" ]
+    if [ "x$ec2_state" = "xstopped" ]
     then
         startEc2
     fi
 
-    while [ "x$STATUS" != "xrunning" ]
+    while [ "x$ec2_state" != "xrunning" ]
     do
         echo -n "."
         sleep 1
-        STATUS=$(statusEc2)
+        statusEc2 true
+        result=$?
+        [ ! $result -eq 0 ] && return $result
     done
 
-    EC2_IP=`aws ec2 describe-instances --region ${AWS_REGION}  --filters ${EC2_FILTERS} --query "Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicIp" | jq -r '.'`
+    ec2_ip=`aws ec2 describe-instances --region ${AWS_REGION}  --filters ${EC2_FILTERS} --query "Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicIp" | jq -r '.'`
 
-    echo "ssh -i ${AWS_KEY} ubuntu@${EC2_IP}"
-    ssh -i ${AWS_KEY} ubuntu@${EC2_IP}
+    echo "ssh -i ${AWS_KEY} ubuntu@${ec2_ip}"
+    ssh -i ${AWS_KEY} ubuntu@${ec2_ip}
 
     read -q "ANSWER?Shutdown instance? " 
     if [ "x$ANSWER" = "xy" ]
@@ -145,4 +121,6 @@ function sshEc2() {
         echo "\n --> Shutting down instance <--"
         stopEc2
     fi
+
+    return 0
 }
