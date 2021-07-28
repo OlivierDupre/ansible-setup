1b82c55cd7959aaadb693318ae142f598f5931b2
Olivier Dupré <dupreolivier@gmail.com>
2021-07-28 11:07:04 +0200

    Added '-m' option on gsutil to speed up the sync

--- backup_sync.sh
+++ backup_sync.sh
@@ -2,5 +2,5 @@
 for dir in Bureau Documents Images
 do
 	echo "$(date) Backing up synchronizing remote gs://petitscoeurs-bkp/$dir with local ~/$dir" >> ~/backup.log
-	gsutil rsync -rd ~/$dir gs://petitscoeurs-bkp/$dir
+	gsutil -m rsync -rd ~/$dir gs://petitscoeurs-bkp/$dir
 done
